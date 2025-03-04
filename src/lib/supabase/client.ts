8664b6cf573d0455b42d96714baf7187e5de2551
Gabriel Dias <gabrieldiassantiago@hotmail.com>
2025-03-04 19:05:04 -0300

    feat: removendo as credenciais

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://kexfmzgscbwkhzrswidl.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtleGZtemdzY2J3a2h6cnN3aWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1MzQ5NjEsImV4cCI6MjA1NjExMDk2MX0.pvRKu88mqEAP7S7SZZWy_h3YSfrhvZB9k66AP_FTzug';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
